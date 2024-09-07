85dda519b37a89711e1f09eedeabf1c62d01853e
Phillip Wellheuser <phillip.wellheuser@gmail.com>
2024-09-06 17:45:59 -0700

    just added some additional testing

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
 
 describe('test the Syllabus tab of the teacher web app', () => {
-  it('goes to index page', () => {
+  it('navs to index page and verifies tabs update', () => {
     cy.visit('https://teacher-phillipmm.replit.app/');
 
     cy.get('.nav-item').should('have.length', 5);
-    // check for each tab
+    // TODO: click each tab to make sure it loads
+
   })
 })
--- cypress/e2e/syllabus.cy.ts
+++ cypress/e2e/syllabus.cy.ts
@@ -1,13 +1,28 @@
 /// <reference types="cypress" />
 
 describe('test the Syllabus tab of the teacher web app', () => {
-  it('goes to index page', () => {
+  it('navs to syllabus tab and verifies presence of UI elements', () => {
     cy.visit('https://teacher-phillipmm.replit.app/');
 
     cy.get('#syllabus-tab').click();
 
     cy.get('#newEntryForm').should('not.be.visible')
     cy.wait(1000);
+    // TODO: check for each form to be invisible
+    // TODO: check for presence of all buttons
+
+  })
+
+  it('verifies behavior of New Assignment button and Form submission', () => {
+    cy.get('#addNewBookButton').click();
+    cy.wait(1000);
+    cy.get('#newEntryForm').should('be.visible')
+    cy.contains('Add New Entry').should('exist');
+    cy.get('#newEntryForm-book').should('exist');
+  })
+
+
+  it('verifies behavior of Add Book button and Form submission', () => {
     cy.get('#addNewBookButton').click();
     cy.wait(1000);
     cy.get('#newEntryForm').should('be.visible')
@@ -26,17 +41,25 @@ describe('test the Syllabus tab of the teacher web app', () => {
     cy.get('#newEntryForm-genre').should('exist');
     cy.contains('Save').should('exist');
     cy.contains('Cancel').should('exist');
-    // click cancel
     cy.contains('Cancel').click();
     cy.get('#newEntryForm').should('not.be.visible');
     cy.get('#addNewBookButton').click();
+
     // fill out form
-    //...
-    // cy.get('Save').click();
+    cy.wait(1000);
+    cy.get('#newEntryForm').should('be.visible')
+    cy.contains('Add New Entry').should('exist');
+    cy.get('#newEntryForm-book').type("New Book Title test");
+    cy.get('#newEntryForm-author').type("Author Name test");
+    cy.get('#newEntryForm-series').type("Series Name test");
+    cy.get('#newEntryForm-num_in_series').type('1');
+    cy.get('#newEntryForm-added_by').type("Tester test");
+    cy.get('#newEntryForm-season').type('1');
+    cy.get('#newEntryForm-is_extra_credit').click();
+    cy.get('#newEntryForm-genre').type("Fiction");
+    cy.get('Save').click();
 
 
-    // make sure pop ups are all invisible
-    // check for presence of all buttons
     // click each button
       // check for popup visibility changes
       // fill out popup and mimic the unit tests
